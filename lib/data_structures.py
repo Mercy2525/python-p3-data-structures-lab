75172261e47ee0401cd39025b6c26fb13b07a6fd
Mercy <mercywmuriithi.mm@gmail.com>
2023-09-25 20:05:12 +0300

    more edits

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,17 +16,33 @@ spicy_foods = [
     },
 ]
 
-def get_names(spicy_foods):
-    pass
+def get_names(spicy_foods): 
+    selected=list() 
+    for food in spicy_foods:
+        selected.append(food['name'])
+    return selected    
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    heat_levels=[food for food in spicy_foods if food['heat_level']>5]
+    return heat_levels
 
+    
 def print_spicy_foods(spicy_foods):
-    pass
+    good_food=list()
+    for food in spicy_foods:
+        heat= "🌶" * food["heat_level"]
+        my_food=(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {heat}')
+        good_food.append(my_food)
+    return good_food
+
+
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    choice_food=[ food for food in spicy_foods if food["cuisine"]==cuisine]
+    print(choice_food)
+    return choice_food
+get_spicy_food_by_cuisine(spicy_foods,'Thai')
 
 def print_spiciest_foods(spicy_foods):
     pass
